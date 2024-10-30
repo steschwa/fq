e4b334fad874b814654cedac81a2c4097db9773b
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2024-10-30 15:52:18 +0100

    feat: print version

--- cmd/root.go
+++ cmd/root.go
@@ -10,18 +10,29 @@ import (
 	"github.com/steschwa/fq/completion"
 )
 
+var (
+	Version   = "0.0.1"
+	CommitSHA = ""
+)
+
 var rootCmd = &cobra.Command{
 	Use:   "fq",
 	Short: "CLI tool to interact with Firestore",
-	Run: func(*cobra.Command, []string) {
-		fmt.Println("please specify a subcommand to run")
-		os.Exit(1)
+	RunE: func(*cobra.Command, []string) error {
+		if PrintVersion {
+			fmt.Printf("version: %v\n", Version)
+			fmt.Printf("commit: %v", CommitSHA)
+			return nil
+		}
+
+		return errors.New("please specify a subcommand to run")
 	},
 }
 
 var (
-	ProjectID string
-	Path      string
+	ProjectID    string
+	Path         string
+	PrintVersion bool
 )
 
 var (
@@ -37,6 +48,8 @@ func init() {
 
 	rootCmd.PersistentFlags().StringVar(&Path, "path", "", "collection or document path")
 	rootCmd.MarkPersistentFlagRequired("path")
+
+	rootCmd.PersistentFlags().BoolVarP(&PrintVersion, "version", "v", false, "print the version")
 
 	c := carapace.Gen(rootCmd)
 	c.Standalone()
